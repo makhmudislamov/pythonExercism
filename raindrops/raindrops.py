fb292ae8c8b15c425902c2566e1f465314fdd440
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-09-21 16:26:29 -0700

    4 tests to pass

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,27 @@
+
+
 def convert(number):
-    pass
+   
+    pling = number % 3
+    plang = number % 5
+    plong = number % 7
+
+    if pling == 0:
+        return("Pling")
+    elif plang == 0:
+        return("Plang")
+    elif plong == 0:
+        return("Plong")
+    elif pling == 0 and plang == 0:
+        return("PlingPlang")
+    elif plang == 0 and plong == 0:
+        return("PlangPlong")
+    elif pling == 0 and plong == 0:
+        return("PlingPlong")
+    elif pling == 0 and plang == 0 and plong == 0:
+        return("PlingPlangPlong")
+    else:
+        return(str(number))
+
+
+
