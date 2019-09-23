4a3d4a6da81ba5ab3d6fcf0ee46712149b3f50a6
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-09-23 16:07:52 -0700

    finished - used all()

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,17 +1,8 @@
+
 import string
 
+
 def is_pangram(sentence):
-    all_printables = string.printable
-    
-    # loop over all_printables
-            # if char is in sentence and all printables
-            # return true
-            # else:
-            # false
-   
-    for char in all_printables:
-        for char in sentence:
-            if char in sentence and char in all_printables:
-                return True
-            else:
-                return False
+    alphabet = list(string.ascii_lowercase)
+    sentence = sentence.lower()
+    return all((char in sentence) for char in alphabet)
