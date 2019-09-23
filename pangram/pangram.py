2ee28ee0c8ffe6d0b0b0b2d62112feb994c3cd7e
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-09-23 15:03:42 -0700

    first attempt - for loop

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,15 @@
+import string
+
 def is_pangram(sentence):
-    pass
+    all_letters = string.ascii_letters
+    
+    # loop over the all_letters
+    # check if the sentences containts all letters from all_letters
+    # if yes: return true
+    # else: return false
+
+    for letter in all_letters:
+        if letter in sentence:
+            return True
+        else:
+            return False
