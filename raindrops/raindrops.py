e0e438dc554e36b2c0aec0719b6a6a2fcc1f2c37
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-09-23 14:04:15 -0700

    passed all tests - submitted

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -6,20 +6,20 @@ def convert(number):
     plang = number % 5
     plong = number % 7
 
-    if pling == 0:
-        return("Pling")
-    elif plang == 0:
-        return("Plang")
-    elif plong == 0:
-        return("Plong")
+    if pling == 0 and plang == 0 and plong == 0:
+        return("PlingPlangPlong")
     elif pling == 0 and plang == 0:
         return("PlingPlang")
     elif plang == 0 and plong == 0:
         return("PlangPlong")
     elif pling == 0 and plong == 0:
         return("PlingPlong")
-    elif pling == 0 and plang == 0 and plong == 0:
-        return("PlingPlangPlong")
+    elif pling == 0:
+        return("Pling")
+    elif plang == 0:
+        return("Plang")
+    elif plong == 0:
+        return("Plong")
     else:
         return(str(number))
 
