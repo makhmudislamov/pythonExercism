42a0791c0b69f897dc7f32e7fa0c17fb69d83d02
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-26 21:48:09 -0800

    generating random names - fixing tests

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,19 +1,30 @@
 import random
-import re
+import string
 
 class Robot(object):
-    def __init__(self):
-        # self.name = name   
+    def __init__(self):  
         pass
 
 
     def reset(self):
+
         # define empty string - name
         # generate random uppercase letters 
         # append to string two of the generated letters
         # fill with random number from 100-999
+        name = ''
 
-        pass
+        for _ in range(2):
+            letters = random.choices(string.ascii_uppercase) # this generates list
+            name += letters[0]
+
+        for _ in range(3):
+            digits = random.choices(string.digits)  # this generates list
+            name += digits[0]
+        return name
 
 
 
+if __name__ == '__main__':
+    rob = Robot()
+    print(rob.reset())
