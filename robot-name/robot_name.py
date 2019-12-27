89f056a46d5bdadb03e1fd532dd3b3d6dea2bdc3
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-26 21:21:37 -0800

    pseudocode

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,3 +1,19 @@
+import random
+import re
+
 class Robot(object):
     def __init__(self):
+        # self.name = name   
         pass
+
+
+    def reset(self):
+        # define empty string - name
+        # generate random uppercase letters 
+        # append to string two of the generated letters
+        # fill with random number from 100-999
+
+        pass
+
+
+
