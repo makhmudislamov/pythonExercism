96476658b05d549982dcf664122754950d161bbe
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-26 22:02:42 -0800

    submitted

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,30 +1,31 @@
 import random
 import string
-
+"""
+Didn't quite understand what other methods should I have and what they should do. Currently it generates random names. 
+I'll revisit
+"""
 class Robot(object):
     def __init__(self):  
         pass
 
+    def name(self, seed = string.ascii_letters):
 
-    def reset(self):
-
-        # define empty string - name
-        # generate random uppercase letters 
-        # append to string two of the generated letters
-        # fill with random number from 100-999
-        name = ''
-
+        robot_name = ''
         for _ in range(2):
-            letters = random.choices(string.ascii_uppercase) # this generates list
-            name += letters[0]
+            letters =  random.choices(seed) # this generates list
+            robot_name += letters[0].upper()
 
         for _ in range(3):
             digits = random.choices(string.digits)  # this generates list
-            name += digits[0]
-        return name
+            robot_name += digits[0]
+        return robot_name
+
+    # def reset(self):
+    #     return self.name(self)
 
 
 
 if __name__ == '__main__':
     rob = Robot()
-    print(rob.reset())
+    print(rob.name())
+    # print(rob.reset())
