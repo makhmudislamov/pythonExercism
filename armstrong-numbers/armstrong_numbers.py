4f3c0d0e70fdc0c0ca3bb18021d0f90cb0401baf
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-29 19:53:19 -0800

    pseudocode

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,16 @@
 def is_armstrong_number(number):
-    pass
+
+    # determine how many digits in the input = power to raise
+    # convert input to string and get its len
+    # declare sum variable
+    # iterate over the stringified input
+    # covert each char to int and raise to the power
+    # add it to sum variable
+    # if total sum is equal to input
+    # return True
+    # else False
+    print(len(str(number)))
+
+
+
+is_armstrong_number(123)
