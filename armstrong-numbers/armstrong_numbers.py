4f728181d9c29dd8eabfd66710ab53446f6b4271
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-29 19:57:47 -0800

    done

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -9,8 +9,16 @@ def is_armstrong_number(number):
     # if total sum is equal to input
     # return True
     # else False
-    print(len(str(number)))
+    string_num = str(number)
+    power = len(string_num)
+    total_sum = 0
+
+    for digit in string_num:
+        total_sum += int(digit) ** power
+        print(total_sum)
+    if total_sum != number:
+        return False
+    return True
 
 
-
-is_armstrong_number(123)
+print(is_armstrong_number(153))
