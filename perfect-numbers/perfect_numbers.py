6b2b59f69180c715c4ee84f64b7db089140fe082
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-30 22:22:23 -0800

    pseudocode

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,2 +1,13 @@
 def classify(number):
+    # declare aliquont sum variable = 0
+    # iterate over the numbers from end not including the number itself:
+    # check if number % num == 0
+    # add it to aliquont sum
+
+    # if to total sum is equal to number:
+    # perfect
+    # if less:
+    # deficient
+    # if more:
+    # abundant
     pass
