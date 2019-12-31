2a47f85f98e6b28474b4f8469830584212beafeb
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-30 22:08:38 -0800

    started this exercise

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,14 +1,6 @@
 def is_armstrong_number(number):
 
-    # determine how many digits in the input = power to raise
-    # convert input to string and get its len
-    # declare sum variable
-    # iterate over the stringified input
-    # covert each char to int and raise to the power
-    # add it to sum variable
-    # if total sum is equal to input
-    # return True
-    # else False
+
     string_num = str(number)
     power = len(string_num)
     total_sum = 0
